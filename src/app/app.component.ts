80e3d1ce5be91693ac7545c383d87be2616f9077
Sergio Romeu <sergioromagr@gmail.com>
2026-08-29 12:54:57 +0200

    feat: Añadir tributo a Tom e incluilo na esquina da páxina web <3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { TomComponent } from './shared/components/tom/tom.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, TomComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.component.css',
---
+++ src/app/shared/components/tom/tom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tom',
+  imports: [],
+  standalone: true,
+  templateUrl: './tom.component.html',
+  styleUrl: './tom.component.css',
+})
+export class TomComponent {}
